1fb33b03cb57c578caf8e4db943e70d04acf6559
Kyle June <github@kylejune.com>
2023-01-29 21:04:09 -0600

    Move middleware to main route

--- example/main.ts
+++ example/main.ts
@@ -1,29 +1,9 @@
 import * as path from "std/path/mod.ts";
-import { Router } from "x/oak/mod.ts";
 import { serve } from "x/udibo_react_app/app_server.tsx";
 
 import route from "./routes/_main.tsx";
-import mainRouter from "./routes/_main.ts";
+import router from "./routes/_main.ts";
 import { AppContext } from "./context.ts";
-
-const router = new Router()
-  .use(async (context, next) => {
-    const { request, response } = context;
-    const start = Date.now();
-    try {
-      await next();
-    } finally {
-      const dt = Date.now() - start;
-      response.headers.set("X-Response-Time", `${dt}ms`);
-      console.log({
-        status: response.status,
-        method: request.method,
-        href: request.url.href,
-        responseTime: dt,
-      });
-    }
-  })
-  .use(mainRouter.routes(), mainRouter.allowedMethods());
 
 await serve({
   port: 9000,
